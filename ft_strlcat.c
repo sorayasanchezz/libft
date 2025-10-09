0051e9a20fc4a12864005c52f221f586c4b33b3e
soraya <sorayasn3@proton.me>
2025-10-09 20:03:26 +0200

    9.10.25

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,38 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: soraya <soraya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/08 18:36:21 by soraya            #+#    #+#             */
-/*   Updated: 2025/10/09 14:14:47 by soraya           ###   ########.fr       */
+/*   Created: 2025/10/09 18:55:16 by soraya            #+#    #+#             */
+/*   Updated: 2025/10/09 19:51:38 by soraya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcpy(char *dst, const char *src, size_t size)
+size_t ft_strlcat(char *dst, const char *src, size_t dsize)
 {
     size_t  i;
+    size_t  j;
+    size_t  temp;
 
     i = 0;
-    while (src[i] && i < (size - 1))
-    {
-        dst[i] = src[i];
+    j = 0;
+    while (dst[i])
         i++;
+    while (src[j])
+        j++;
+    if (dsize <= i)
+        return (dsize + j);
+    temp = i + j;
+    j = 0;
+    while (src[j] && j < (dsize - i - 1))
+    {
+        dst[i] = src[j];
+        i++;
+        j++;
     }
     dst[i] = 0;
-    i = 0;
-    while (src[i])
-        i++;
-    return(i);
+    return (temp);
 }
-/* 
-int main(void)
-{
-    char    str[5] = "HAdss";
-    printf("%ld \n", strlcpy(str, "Holas", 5));
-    printf("%ld \n", ft_strlcpy(str, "Hlass", 5));
-}
- */
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: soraya <soraya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 16:25:21 by sosanche          #+#    #+#             */
-/*   Updated: 2025/10/08 19:01:19 by soraya           ###   ########.fr       */
+/*   Updated: 2025/10/09 19:50:55 by soraya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	if (!n)
+		return (0);
+	
 	size_t	i;
-	int		comp;
 
 	i = 0;
-	while (i < n && s1[i] && s2[i])
+	while (i < n && (s1[i] || s2[i]))
 	{
-		comp = s1[i] - s2[i];
-		if (comp != 0)
-			return (comp);
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
@@ -30,8 +31,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 // int	main(void)
 // {
-// 	char	s1[4] = "Mama";
-// 	char	s2[4] = "Mala";
+// 	char	s1[4] = "Pez";
+// 	char	s2[4] = "Ojo";
 // 	size_t	tam = 3;
 // 	printf("original: %d \n", strncmp(s1, s2, tam));
 // 	printf("yo: %d", ft_strncmp(s1, s2, tam));
